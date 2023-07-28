052d84e466ec6991a5c333a6369d1d6fe2c82e51
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-07-28 17:39:51 +0200

    Add spring application AfterDarkAPI

---
+++ AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/AfterDarkApiApplication.java
@@ -0,0 +1,13 @@
+package com.nikolaev.AfterDarkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterDarkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterDarkApiApplication.class, args);
+	}
+
+}
---
+++ AfterDarkAPI/src/test/java/com/nikolaev/AfterDarkAPI/AfterDarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolaev.AfterDarkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterDarkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
