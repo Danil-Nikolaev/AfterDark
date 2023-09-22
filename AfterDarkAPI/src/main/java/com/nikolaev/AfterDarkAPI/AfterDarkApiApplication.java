ea0df379101dc693e4bc97dbe7f265727336d32b
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-09-22 13:42:07 +0300

    Add config for localhost:5050

--- AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/AfterDarkApiApplication.java
+++ AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/AfterDarkApiApplication.java
@@ -2,12 +2,21 @@ package com.nikolaev.AfterDarkAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class AfterDarkApiApplication {
+public class AfterDarkApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AfterDarkApiApplication.class, args);
 	}
 
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://127.0.0.1:5500") // Разрешенный источник (ваш фронтенд)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
+    }
 }
