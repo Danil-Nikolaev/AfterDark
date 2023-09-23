c36b3f3c8288a5e12e9daaddef95616595de0303
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-09-23 12:07:00 +0300

    Add new method fo dind objects by id

--- AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/repositories/CandleRepository.java
+++ AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/repositories/CandleRepository.java
@@ -1,5 +1,7 @@
 package com.nikolaev.AfterDarkAPI.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.nikolaev.AfterDarkAPI.models.Candle;
 
 @Repository
 public interface CandleRepository extends CrudRepository<Candle, Long> {
-    
+    List<Candle> findAllByWickId(long id);
+    List<Candle> findAllByWaxId(long id);
+    List<Candle> findAllBySmellId(long id);
+    List<Candle> findAllByShapeId(long id);
+    List<Candle> findAllByColorShapeId(long id);
 }
