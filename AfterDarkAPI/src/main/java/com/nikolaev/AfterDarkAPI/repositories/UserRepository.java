dabf69a9458adc63456cde473ac27f9e6a9c5fd0
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-10-16 20:26:31 +0300

    add method findByLogin in UserRepository

--- AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/repositories/UserRepository.java
+++ AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.nikolaev.AfterDarkAPI.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.nikolaev.AfterDarkAPI.models.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    
+    Optional<User> findByLogin(String login);
 }
