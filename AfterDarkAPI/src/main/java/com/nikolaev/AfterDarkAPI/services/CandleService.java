09a9923364feb12e2482e11decffc134a986d3ff
Danil-Nikolaev <danil_nikolaev_96@bk.ru>
2023-10-16 20:28:22 +0300

    add method findCandle in CandleService

--- AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/services/CandleService.java
+++ AfterDarkAPI/src/main/java/com/nikolaev/AfterDarkAPI/services/CandleService.java
@@ -4,18 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.nikolaev.AfterDarkAPI.models.Candle;
+import com.nikolaev.AfterDarkAPI.models.ColorShape;
+import com.nikolaev.AfterDarkAPI.models.Shape;
+import com.nikolaev.AfterDarkAPI.models.Smell;
+import com.nikolaev.AfterDarkAPI.models.Wick;
 import com.nikolaev.AfterDarkAPI.repositories.CandleRepository;
 
 @Service
 public class CandleService {
     private CandleRepository candleRepository;
+    private WickService wickService;
+    private SmellService smellService;
+    private ShapeService shapeService;
+    private ColorShapeService colorShapeService;
 
-    public CandleService(@Autowired CandleRepository candleRepository) {
+    
+
+    public CandleService(CandleRepository candleRepository, WickService wickService, SmellService smellService,
+            ShapeService shapeService, ColorShapeService colorShapeService) {
         this.candleRepository = candleRepository;
+        this.wickService = wickService;
+        this.smellService = smellService;
+        this.shapeService = shapeService;
+        this.colorShapeService = colorShapeService;
     }
 
     public List<Candle> index() {
@@ -55,5 +70,30 @@ public class CandleService {
 
     public void delete(long id) {
         candleRepository.deleteById(id);
+    }
+
+    public Candle findCandle(JsonNode candle) {
+        Smell smell = this.smellService.show(Long.valueOf(candle.get("smell").asText()));
+        Shape shape = this.shapeService.show(Long.valueOf(candle.get("shape").asText()));
+        Wick wick = this.wickService.show(Long.valueOf(candle.get("wick").asText()));
+        ColorShape colorShape = this.colorShapeService.show(Long.valueOf(candle.get("colorShape").asText()));
+        int price = smell.getPrice() + shape.getPrice() + wick.getPrice() + colorShape.getPrice();
+
+        Candle localCandle = new Candle();
+        localCandle.setColorShape(colorShape);
+        localCandle.setShape(shape);
+        localCandle.setWick(wick);
+        localCandle.setSmell(smell);
+        localCandle.setCustom(true);
+        localCandle.setName(candle.get("name").asText());
+        localCandle.setPrice(price);
+
+        List<Candle> candles = index();
+        for (Candle cyclCandle : candles) {
+            if (cyclCandle.equals(localCandle)) {
+                return show(cyclCandle.getId());
+            }
+        }
+        return save(localCandle);
     }
 }
